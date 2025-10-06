5a75d5cbf4f5bef052ebcf8493181e418719f331
Truong Quang Huy <youremail@example.com>
2025-10-06 19:54:04 +0700

    interface client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,13 @@
-module.exports.index = (req, res) => {
+const Products = require("../../models/product.model.js");
+
+module.exports.index = async (req, res) => {
+  const products = await Products.find({
+    // stock: { $gte: 70 }, // >= 70.
+  });
+  console.log("products:", products);
+
   res.render("client/pages/products/index.pug", {
     title: "Product Page",
+    products,
   });
 };
