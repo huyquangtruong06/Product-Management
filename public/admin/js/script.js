6786cf4fc28a06a30d4cc6e9b95f517338275e3e
Truong Quang Huy <youremail@example.com>
2025-10-11 22:39:34 +0700

    search

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -17,3 +17,20 @@ if (listButtonStatus.length > 0) {
     });
   }
 }
+
+const formSearch = document.querySelector("#form-search");
+if (formSearch) {
+  let url = new URL(window.location.href); // a new url
+
+  formSearch.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const keyword = event.target.elements.keyword.value;
+
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+    window.location.href = url.href; // assign a new url for old url
+  });
+}
